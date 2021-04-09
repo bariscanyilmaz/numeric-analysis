3f07d17ef7c178f645c4b26d52eb3f98dde22fe4
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2021-04-09 20:32:08 +0300

    initial commit

---
+++ functions.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "functions.h"
+
+void calculate()
+{
+    printf("Hello Everyone \n");
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include "functions.h"
+
+int main(){
+    
+    calculate();
+    
+    return 0;
+}
